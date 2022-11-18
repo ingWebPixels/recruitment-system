3e8eefdeead443576278c771feda8df70cde58cf
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-11-17 22:28:07 -0500

    feat: use role in frontend

---
+++ components/PrivateComponent.tsx
@@ -0,0 +1,23 @@
+import { Enum_RoleName } from '@prisma/client';
+import { useSession } from 'next-auth/react';
+
+interface PrivateComponentProps {
+  roleList: Enum_RoleName[];
+  children: JSX.Element;
+}
+
+const PrivateComponent = ({ roleList, children }: PrivateComponentProps) => {
+  const { data: session } = useSession();
+
+  const roleCheck = session?.user?.roles?.some((role) =>
+    roleList.includes(role.name)
+  );
+
+  if (roleCheck) {
+    return children;
+  }
+
+  return null;
+};
+
+export default PrivateComponent;
--- pages/unauthorized.tsx
+++ pages/unauthorized.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { Enum_RoleName } from '@prisma/client';
+import PrivateComponent from '@components/PrivateComponent';
 
 const UnauthorizedPage = () => {
   const { data: session, status } = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
 
   if (status === 'loading') {
     return <div> Cargando... </div>;
@@ -25,6 +31,14 @@ const UnauthorizedPage = () => {
           Cerrar sesión
         </button>
       )}
+
+      <PrivateComponent roleList={[Enum_RoleName.ADMIN]}>
+        <div>Esto solo lo ve un administrador </div>
+      </PrivateComponent>
+
+      <PrivateComponent roleList={[Enum_RoleName.CANDIDATE]}>
+        <div>Esto solo lo ve un Candidato </div>
+      </PrivateComponent>
 
       <p>Usted no está autorizado para acceder al recurso solicitad.</p>
     </div>
